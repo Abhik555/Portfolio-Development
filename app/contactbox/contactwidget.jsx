f2423fe3d0fef5c59199d2390854c06e1ad5f5f2
Abthedev <42204991+Abhik555@users.noreply.github.com>
2024-05-24 16:50:30 +0530

    fixed form

--- app/contactbox/contactwidget.jsx
+++ app/contactbox/contactwidget.jsx
@@ -5,14 +5,7 @@ export default function ContactWidget() {
     event.preventDefault()
 
     const formData = new FormData(event.target);
-    const response = await fetch("/api/submit", {
-      method: "POST",
-      body: formData,
-    });
-
-    // Handle response if necessary
-    const data = await response;
-    console.log(data)
+    //to be implimented
   }
 
   return (
